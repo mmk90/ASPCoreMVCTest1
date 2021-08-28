5c6eaa6eee8e5bb429895a8499a272426a978ae9
Mahdi <mmk90@yahoo.com>
2021-08-28 18:01:12 +0430

    شششش

---
+++ Website/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+        [Required]
+        public DateTime RegisterDate { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
